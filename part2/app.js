1d7ddba0aa50abc0013ece6a6d4385cdebb60940
tomytud <60183624+tomytud@users.noreply.github.com>
2021-10-07 10:02:30 +0200

    Update app.js

--- part2/app.js
+++ part2/app.js
@@ -28,7 +28,7 @@ cameraTrigger.onclick = function() {
     cameraSensor.width = cameraView.videoWidth;
     cameraSensor.height = cameraView.videoHeight;
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-    cameraOutput.src = cameraSensor.toDataURL("image/test");
+    cameraOutput.src = cameraSensor.toDataURL("image/webp");
     cameraOutput.classList.add("taken");
     //track.stop();
 };
